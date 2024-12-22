54f52a2206db8b1aeb0484a7d7e256a1f3605506
Ryan Chen <34699593+shezimanor@users.noreply.github.com>
2024-12-22 17:07:11 +0800

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex w-full min-w-96 flex-col">
+    <div class="h-screen pt-14">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
